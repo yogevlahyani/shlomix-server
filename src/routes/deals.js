37c666f0fd39d205cc39714e0008048eebd863fd
yogevlahyani <yogevlahyani@gmail.com>
2018-03-15 19:50:05 +0200

    debug

--- src/routes/deals.js
+++ src/routes/deals.js
@@ -19,11 +19,7 @@ module.exports = () => {
       let items = [];
 
       deals.forEach(deal => {
-        console.log(deal.item.category[0]);
-        console.log(req.params.catId);
-        console.log(deal.item.category[0] == req.params.catId);
-        if (deal.item.category[0] === req.params.catId) { items.push(deal) }
-        console.log(items);
+        if (deal.item.category[0] == req.params.catId) { items.push(deal) }
       });
 
       console.log(items);
